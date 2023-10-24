a4e23e1048a7c34fa7bde6b701b502a49e7c91d3
avof1ow <avof1ow4@gmail.com>
2023-10-24 18:48:40 +0000

    исправил согласно требованием

--- test.py
+++ test.py
@@ -1,61 +1,50 @@
-import requests
 import pandas as pd
+import requests
 
-current_date = {"day": 22, "month": 10, "year": 2023}  # вчерашняя дата или раньше
-data = {"rouble": [], "date": []}
-for _ in range(int(input("На сколько дней\n>>>"))):
-    str_day = str(current_date["day"])
-    str_month = str(current_date["month"])
-    str_year = str(current_date["year"])
-    if len(str_day) == 1:
-        str_day = "0" + str_day
-    if len(str_month) == 1:
-        str_month = "0" + str_month
-    data_json = requests.get(
-        f"https://www.cbr-xml-daily.ru/archive/{str_year}/{str_month}/{str_day}/daily_json.js"
-    ).json()
-    try:
-        data["rouble"].append(data_json["Valute"]["USD"]["Value"])
-        data["date"].append(data_json["Date"])
-    except:
-        data["rouble"].append("Курс не найден")
-        data["date"].append(f"{str_year}-{str_month}-{str_day}")
-    if current_date["day"] == 1:
-        if current_date["month"] == 1:
-            current_date["month"] = 12
-            current_date["year"] -= 1
-        else:
-            current_date["month"] -= 1
-        match current_date["month"]:
-            case 1:
-                current_date["day"] = 31
-            case 2:
-                if current_date["year"] % 4 == 0:
-                    current_date["day"] = 28
+
+def get_data(day: int, month: int, year: int, days_count: int = 30):
+    data = {"rouble": [], "date": []}
+    for _ in range(days_count):
+        str_day = str(day)
+        str_month = str(month)
+        str_year = str(year)
+        if len(str_day) == 1:
+            str_day = "0" + str_day
+        if len(str_month) == 1:
+            str_month = "0" + str_month
+        data_json = requests.get(
+            f"https://www.cbr-xml-daily.ru/archive/{str_year}/{str_month}/{str_day}/daily_json.js"
+        ).json()
+        try:
+            data["rouble"].append(data_json["Valute"]["USD"]["Value"])
+            data["date"].append(data_json["Date"][0:-15])
+        except:
+            data["rouble"].append("-")
+            data["date"].append(f"{str_year}-{str_month}-{str_day}")
+        if day == 1:
+            if month == 1:
+                month = 12
+                year -= 1
+            else:
+                month -= 1
+            if month in (1, 3, 5, 7, 8, 10, 12):
+                day = 31
+            elif month in (4, 6, 9, 11):
+                day = 30
+            elif month == 2:
+                if year % 4 == 0:
+                    day = 28
                 else:
-                    current_date["day"] = 29
-            case 3:
-                current_date["day"] = 31
-            case 4:
-                current_date["day"] = 30
-            case 5:
-                current_date["day"] = 31
-            case 6:
-                current_date["day"] = 30
-            case 7:
-                current_date["day"] = 31
-            case 8:
-                current_date["day"] = 31
-            case 9:
-                current_date["day"] = 30
-            case 10:
-                current_date["day"] = 31
-            case 11:
-                current_date["day"] = 30
-            case 12:
-                current_date["day"] = 31
-    else:
-        current_date["day"] -= 1
-df = pd.DataFrame(data)
-df. to_csv("dollar_course.csv", sep=",", encoding="utf-8") 
-  
+                    day = 29
+        else:
+            day -= 1
+    return data
+
+
+def create_CSV(data: dict, to: str):
+    df = pd.DataFrame(data)
+    df.to_csv(to, sep=",", encoding="utf-8")
+
+
+output_data = get_data(23, 10, 2023, 30)
+create_CSV(output_data, "dollar_course.csv")
